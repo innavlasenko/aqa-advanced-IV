2da2b94b46dd49c3e5edc7d1cbd7224b17c50c8f
Inna Vlasenko <ivashchenkoinnaivan@gmail.com>
2024-05-31 20:33:35 +0300

    add Promices

---
+++ jsPromices/task11.1.mjs
@@ -0,0 +1,9 @@
+const planetsName = []
+for (let i = 1; i < 11; i++) {
+    const result = await fetch(`https://swapi.dev/api/planets/${i}`)
+    const body = await result.json()
+    planetsName.push(body.name)
+}
+console.log(planetsName);
+
+
